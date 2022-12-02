eea2061d4c4631450d2d3aba95b960308540411f
anri103 <ianrianri@gmail.com>
2022-12-03 01:32:23 +0300

    modify styles

--- js/main.js
+++ js/main.js
@@ -3,8 +3,34 @@
 
 })(jQuery);
 
-// Smooth Scroll and Spy Scrolling JS
+// Back to Top Button
+//Get the button
+const mybutton = document.getElementById('back-top');
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 400 ||
+        document.documentElement.scrollTop > 400
+    ) {
+        mybutton.classList.add('show');
+    } else {
+        mybutton.classList.remove('show');
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Smooth Scroll and Spy Scrolling JS
 const makeNavLinksSmooth = () => {
     const navLinks = document.querySelectorAll('.smooth-link');
 
@@ -31,15 +57,14 @@ const spyScrolling = () => {
         for (let s in sections)
             if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos + 300) {
                 const id = sections[s].id;
-                document.querySelector('.sidebar-nav .active').classList.remove('active');
-                document.querySelector(`a[href*=${id}]`).parentNode.classList.add('active');
+                document.querySelector('.sidebar-nav .smooth-link.active').classList.remove('active');
+                document.querySelector(`a[href*=${id}]`).classList.add('active');
             }
     }
 }
 spyScrolling();
 
 // Слайдер на странице index.html
-
 const activitySwiper = new Swiper('.activitySwiper', {
     slidesPerView: 'auto',
     spaceBetween: 30,
@@ -52,7 +77,6 @@ const activitySwiper = new Swiper('.activitySwiper', {
 });
 
 // Слайдер на странице index.html
-
 const testimonialSwiper = new Swiper('.testimonialSwiper', {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -68,7 +92,6 @@ const testimonialSwiper = new Swiper('.testimonialSwiper', {
 });
 
 // Слайдер на странице project-item.html
-
 const projectSwiper = new Swiper('.projectSwiper', {
     slidesPerView: 'auto',
     spaceBetween: 30,
@@ -81,7 +104,6 @@ const projectSwiper = new Swiper('.projectSwiper', {
 });
 
 // Слайдер на странице project-item.html
-
 const worksSwiper = new Swiper('.worksSwiper', {
     slidesPerView: 3,
     spaceBetween: 10,
@@ -109,7 +131,6 @@ const worksSwiper = new Swiper('.worksSwiper', {
 });
 
 // Слайдер на странице company.html
-
 const companyGallerySwiper = new Swiper('.companyGallerySwiper', {
     slidesPerView: 'auto',
     spaceBetween: 20,
@@ -127,38 +148,31 @@ const companyGallerySwiper = new Swiper('.companyGallerySwiper', {
 
 
 // Попап видео на index.html
-
 const lightboxMainVideo = GLightbox({
     selector: '.main-video-glightbox1'
 });
 
 // Попап видеоотзывов
-
 const lightboxTestimonial = GLightbox({
     selector: '.testimonial-glightbox1'
 });
 
 // Попап галереи на project-item.html
-
 const lightboxProjectItem = GLightbox({
     selector: '.project-item-glightbox1'
 });
 
 // Попап галереи на company.html
-
 const lightboxCompany = GLightbox({
     selector: '.glightbox1'
 });
 
 // Попап изображений на blog-item.html
-
 const lightboxBlogItem = GLightbox({
     selector: '.blog-item-glightbox1'
 });
 
-
 // PHONE MASK
-
 // const phoneMask = IMask(
 //     document.getElementById('InputPhone1'), {
 //     mask: '+{7}(000)000-00-00'
