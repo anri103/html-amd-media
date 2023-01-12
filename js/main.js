caa03a9227a0e11de786be7853f1b4dd505ab000
anri103 <ianrianri@gmail.com>
2023-01-12 16:35:50 +0300

    Collapse fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 //////////////////////////////////////////////////////////////////
 // [ Collapse responsive navbar when toggler is visible ]
-const navbarToggler = document.body.querySelector('.navbar-toggler');
-const responsiveNavItems = [].slice.call(
-    document.querySelectorAll('#navbarNav .nav-link')
-);
-responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-        if (window.getComputedStyle(navbarToggler).display !== 'none') {
-            navbarToggler.click();
-        }
-    });
-});
+// const navbarToggler = document.body.querySelector('.navbar-toggler');
+// const responsiveNavItems = [].slice.call(
+//     document.querySelectorAll('#navbarNav .nav-link')
+// );
+// responsiveNavItems.map(function (responsiveNavItem) {
+//     responsiveNavItem.addEventListener('click', () => {
+//         if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//             navbarToggler.click();
+//         }
+//     });
+// });
 
 //////////////////////////////////////////////////////////////////
 // [ Headroom ]
