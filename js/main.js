d7f4a85d45a8d32d0215f37ebb5da235219c4cbb
anri103 <ianrianri@gmail.com>
2023-07-17 18:10:34 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -49,67 +49,67 @@ scrollBtn.addEventListener('click', () => {
 
 //////////////////////////////////////////////////////////////////
 // [ Smooth Scroll ]
-const makeNavLinksSmooth = () => {
-    const navLinks = document.querySelectorAll('.smooth-link');
+// const makeNavLinksSmooth = () => {
+//     const navLinks = document.querySelectorAll('.smooth-link');
 
-    for (let n in navLinks) {
-        if (navLinks.hasOwnProperty(n)) {
-            navLinks[n].addEventListener('click', e => {
-                e.preventDefault();
-                document.querySelector(navLinks[n].hash)
-                    .scrollIntoView({
-                        behavior: "smooth"
-                    });
-            });
-        }
-    }
-}
-makeNavLinksSmooth();
+//     for (let n in navLinks) {
+//         if (navLinks.hasOwnProperty(n)) {
+//             navLinks[n].addEventListener('click', e => {
+//                 e.preventDefault();
+//                 document.querySelector(navLinks[n].hash)
+//                     .scrollIntoView({
+//                         behavior: "smooth"
+//                     });
+//             });
+//         }
+//     }
+// }
+// makeNavLinksSmooth();
 
 //////////////////////////////////////////////////////////////////
 // [ Spy Scrolling ]
-const spyScrolling = () => {
-    const sections = document.querySelectorAll('.section-spy');
+// const spyScrolling = () => {
+//     const sections = document.querySelectorAll('.section-spy');
 
-    window.onscroll = () => {
-        const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+//     window.onscroll = () => {
+//         const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
 
-        for (let s in sections)
-            if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos + 300) {
-                const id = sections[s].id;
-                document.querySelector('.sidebar-nav .smooth-link.active').classList.remove('active');
-                document.querySelector(`a[href*=${id}]`).classList.add('active');
-            }
-    }
-}
-spyScrolling();
+//         for (let s in sections)
+//             if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos + 300) {
+//                 const id = sections[s].id;
+//                 document.querySelector('.sidebar-nav .smooth-link.active').classList.remove('active');
+//                 document.querySelector(`a[href*=${id}]`).classList.add('active');
+//             }
+//     }
+// }
+// spyScrolling();
 
 //////////////////////////////////////////////////////////////////
 // [ Swiper ]
-const activitySwiper = new Swiper('.activitySwiper', {
-    slidesPerView: 1.15,
-    spaceBetween: 15,
-    freeMode: true,
-    mousewheel: true,
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: true,
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 1.5,
-            spaceBetween: 20,
-        },
-        992: {
-            slidesPerView: 1.75,
-            spaceBetween: 30,
-        },
-        1200: {
-            slidesPerView: 2.15,
-            spaceBetween: 30,
-        },
-    },
-});
+// const activitySwiper = new Swiper('.activitySwiper', {
+//     slidesPerView: 1.15,
+//     spaceBetween: 15,
+//     freeMode: true,
+//     mousewheel: true,
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//         hide: true,
+//     },
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 1.5,
+//             spaceBetween: 20,
+//         },
+//         992: {
+//             slidesPerView: 1.75,
+//             spaceBetween: 30,
+//         },
+//         1200: {
+//             slidesPerView: 2.15,
+//             spaceBetween: 30,
+//         },
+//     },
+// });
 
 const testimonialSwiper = new Swiper('.testimonialSwiper', {
     slidesPerView: 1,
@@ -193,6 +193,30 @@ const teamSwiper = new Swiper('.teamSwiper', {
     },
 });
 
+const blogSwiper = new Swiper('.blogSwiper', {
+    slidesPerView: 1.25,
+    spaceBetween: 15,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1600: {
+            slidesPerView: 4,
+            spaceBetween: 72,
+        },
+    },
+});
+
 //////////////////////////////////////////////////////////////////
 // [ GLightbox ]
 const lightboxMainVideo = GLightbox({
@@ -215,9 +239,44 @@ const lightboxBlogItem = GLightbox({
     selector: '.glightbox-post-01'
 });
 
+
+
 //////////////////////////////////////////////////////////////////
 // [ PHONE MASK ]
-const phoneMask = IMask(
-    document.getElementById('InputPhone1'), {
-    mask: '+{7}(000)000-00-00'
+// const phoneMask = IMask(
+//     document.getElementById('InputPhone1'), {
+//     mask: '+{7}(000)000-00-00'
+// });
+
+var maskPhone = document.querySelectorAll('.maskPhone')
+var maskDate = document.querySelectorAll('.maskDate')
+
+maskPhone.forEach(function(el) {
+    IMask(el, {
+        mask: '+{7}(000)000-00-00'
+    });
+});
+
+maskDate.forEach(function(el) {
+    IMask(el, {
+        mask: Date,
+        min: new Date(1900, 0, 1),
+        lazy: false
+    });
+});
+
+//////////////////////////////////////////////////////////////////
+// [ Enable Fancybox ]
+
+Fancybox.bind("[data-fancybox]", {
+    Thumbs: {
+        type: "classic",
+    },
+    Toolbar: {
+        display: {
+          left: [],
+          middle: ["close"],
+          right: [],
+        },
+    },
 });
